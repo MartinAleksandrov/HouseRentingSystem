c242f65376748e532f4c77d346925cb88adfdd02
Martin Aleksandrov <martin_20.05@abv.bg>
2024-02-23 20:07:16 +0200

    Service collection added to simplify Program.cs

---
+++ HouseRentingSystem/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,36 @@
+﻿namespace HouseRentingSystem.Extensions
+{
+    using HouseRentingSystem.Data;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.EntityFrameworkCore;
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddAplicationServices(this IServiceCollection services)
+        {
+            return services;
+        }
+
+        public static IServiceCollection AddAplicationDbContext(this IServiceCollection services,IConfiguration config)
+        {
+            var connectionString = config
+                .GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            services.AddDatabaseDeveloperPageExceptionFilter();
+
+
+            return services;
+        }
+
+        public static IServiceCollection AddAplicationIdentity(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDefaultIdentity<IdentityUser>(options =>
+                           options.SignIn.RequireConfirmedAccount = false)
+               .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            return services;
+        }
+    }
+}
